93995508f690943c3a3c4cae9fd5ea95a47d1c5e
Jay Patel <jpatel2049@gmail.com>
2019-09-17 18:58:23 -0400

    Added the button that is centered and when clicked it changes the text in the text view to "You have clicked the button."

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,27 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button myButton;
+    String text = "You have clicked the button.";
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = findViewById(R.id.button);
+        textview = findViewById(R.id.textview);
+
+        myButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                textview.setText(text);
+            }
+        });
     }
 }
